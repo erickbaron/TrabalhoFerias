a8e26883b7e359a9f6516a7f20fd1f0b6d1e9ad1
Erick Baron <erickbaron123456@gmail.com>
2019-07-14 15:35:59 -0300

    Initial commit

---
+++ Repository/Estrutura.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE estados(
+id INT PRIMARY KEY IDENTITY (1,1),
+nome VARCHAR (50),
+sigla VARCHAR (2)
+);
+
+CREATE TABLE cidades (
+id INT PRIMARY KEY IDENTITY(1,1),
+
+id_estado INT,
+FOREIGN KEY (id_estado) REFERENCES estado(id), 
+
+nome VARCHAR(50),
+numero_habitantes INT
+);
+
+CREATE TABLE clientes(
+id INT PRIMARY KEY IDENTITY(1,1),
+
+id_cidade INT,
+FOREIGN KEY (id_cidade) REFERENCES cidade(id),
+
+nome VARCHAR(50),
+cpf VARCHAR(50),
+data_nascimento DATETIME2(7),
+numero INT,
+complemento NCHAR(10),
+logradouro NCHAR(10),
+cep NCHAR(10),
+
+);
