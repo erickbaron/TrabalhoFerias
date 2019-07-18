6cfd4b0640e52db6d94b5c96201fd2a956486382
JoaoVictorDF <joaovdf17@gmail.com>
2019-07-18 16:08:39 -0300

    Criada a Conexão, Feito CategoriaRepository, iniciada TarefaRepository

---
+++ Repository/TarefaRepository.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class TarefaRepository
+    {
+        public int Inserir(Tarefa tarefa)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"INSERT INTO tarefas
+(id_usuario_responsavel, id_projeto, id_categoria, titulo, descricao, duracao)
+OUTPUT INSERTED.ID VALUES
+(@ID_USUARIO_RESPONSAVEL, @ID_PROJETO, @ID_CATEGORIA, @TITULO, @DESCRICAO, @DURACAO)";
+            comando.Parameters.AddWithValue("@ID_USUARIO_RESPONSAVEL", tarefa.IdUsuarioResponsavel);
+            comando.Parameters.AddWithValue("@ID_PROJETO", tarefa.Projeto);
+            comando.Parameters.AddWithValue("@ID_CATEGORIA", tarefa.Categoria);
+            comando.Parameters.AddWithValue("@TITULO", tarefa.Titulo);
+            comando.Parameters.AddWithValue("@DESCRICAO", tarefa.Descricao);
+            comando.Parameters.AddWithValue("@DURACAO", tarefa.Duracao);
+            int id = Convert.ToInt32(comando.ExecuteScalar());
+            comando.Connection.Close();
+            return id;
+        }
+    }
+}
